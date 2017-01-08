d7dcb8e1590fbd14b3cc35ae7a9b33754176358e
introp7 <simonasmorkys@gmail.com>
2017-01-08 18:25:27 +0200

    "i" something

--- sqlClass.php
+++ sqlClass.php
@@ -1,1 +1,15 @@
 <?php
+define ('HOST', 'localhost');
+define ('USERNAME', 'root');
+define('PASSWORD', "");
+define ('DB', 'darbuotojai');
+define ('CHARSET', 'utf8');
+
+class MySQLDB {
+	static $connect=NULL;
+	private static function connect(){
+		self::$connect= mysqli_connect( HOST, USERNAME, PASSWORD);
+		mysqli_select_db (self::$connect, DB);
+		mysqli_set_charset(self::$connect, CHARSET);
+
+	}
--- sqlRaw.php
+++ sqlRaw.php
@@ -1,1 +1,7 @@
 <?php
+
+$con=mysqli_connect('localhost','root','');
+mysqli_select_db($con, 'darbuotojai');
+mysqli_set_charset($con, 'utf8');
+
+mysqli_close($con);
